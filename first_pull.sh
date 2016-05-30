0fe331d034698bfa0e6e2c5360492b5f9c67cbaa
aokiyuya <yuya.aoki@fujielab.org>
2016-05-30 14:15:04 +0900

    fix first_pull.sh

--- first_pull.sh
+++ first_pull.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 cd ~/
-sh myvimrc/pull_vimrc.sh
+if [ "$(uname)" == 'Darwin' ]; then
+	OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+	OS='Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+	OS='Cygwin'
+else
+	echo "Your platform ($(uname -a)) is not supported."
+	exit 1
+fi
+if [OS == 'Mac']; then
+	pullvimrc
+elif [OS == 'Linux']; then
+	sh myvimrc/pull_vimrc.sh
+fi
 rm -rf .vim/bundle/
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
